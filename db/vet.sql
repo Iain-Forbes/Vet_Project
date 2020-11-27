40f99d28fbdc4f1d5ebb59caa21b20a7d232601d
Iain Forbes <i.forbes77@gmail.com>
2020-11-27 18:18:17 +0000

    Featuring, New Diagram, model updates and datebase works

--- db/vet.sql
+++ db/vet.sql
@@ -1,27 +1,28 @@
-DROP TABLE IF EXISTS vets;
 DROP TABLE IF EXISTS animals;
+DROP TABLE IF EXISTS vets;
 DROP TABLE IF EXISTS owners;
 
-CREATE TABLE owners; (
-    id SERIAL PRIMARY KEY
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
     name VARCHAR(225),
-    contact_details VARCHAR(225),
-)
+    contact_details VARCHAR(225)
+);
 
-CREATE TABLE animals; (
-    id SERIAL PRIMARY KEY
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(225),
+    contact_details VARCHAR(225)
+);
+
+CREATE TABLE animals (
+    id SERIAL PRIMARY KEY,
     name VARCHAR(225),
     date_of_birth VARCHAR(225),
-    animal_type VARCHAR(225)
-    treatment_notes TEXT
-    owner_id SERIAL REFERENCES owner(id),
-    vet_id SERIAL REFERENCES vet(id),
+    animal_type VARCHAR(225),
+    treatment_notes TEXT,
+    vet_id SERIAL REFERENCES vets(id),
+    owner_id SERIAL REFERENCES owners(id)
     
-)
+);
 
-CREATE TABLE vets; (
-    id SERIAL PRIMARY KEY
-    name VARCHAR(225),
-    contact_details VARCHAR(225),
-)
 
--- models/vet.py
+++ models/vet.py
@@ -1,4 +1,4 @@
 class Vet:
-    def __init__(self, name, contact_details, id=None ):
-        self.name = name
-        self.contact_details = contact_details
+    def __init__(self,  appointment_time, appointment_date, id=None ):
+        self.appointment_time = appointment_time
+        self.appointment_date = appointment_date
