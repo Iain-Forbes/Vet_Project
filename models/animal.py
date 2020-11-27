a1880861de1c3e8dda119b7d79b9c062b834e960
Iain Forbes <i.forbes77@gmail.com>
2020-11-27 15:23:11 +0000

    Added models

---
+++ models/animal.py
@@ -0,0 +1,6 @@
+class Animal:
+    def __init__(self, date_of_birth, animal_type, treatment_notes, id=None):
+        self.date_of_birth = date_of_birth
+        self.animal_type = animal_type
+        self.treatment_notes = treatment_notes
+
---
+++ models/owner.py
@@ -0,0 +1,4 @@
+class Owner:
+    def __init__(self, name, contact_details, id=None):
+        self.name = name
+        self.contact_details = contact_details
