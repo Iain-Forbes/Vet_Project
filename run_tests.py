aef7321f6ddb11aae68e756e78b5ef39a0bf3d14
Iain Forbes <i.forbes77@gmail.com>
2020-11-29 12:59:05 +0000

    started to tidy up some CSS

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,7 @@
 import unittest
  
 from tests.owner_test import OwnerTest
-from tests.appointment_test import AppointmentTest
+from tests.appointments_test import AppointmentTest
 from tests.animal_test import AnimalTest
 
 
