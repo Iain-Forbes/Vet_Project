0efa19ce6759b90ae27d2eac068ab403c732f9a3
Iain Forbes <i.forbes77@gmail.com>
2020-12-01 12:49:00 +0000

    Made some CCS/HTML edits, added stats to home page

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, Blueprint
 
+import repositories.animal_repository as animal_repository
+import repositories.owner_repository as owner_repository
+import repositories.appointment_repository as appointment_repository
+
 from controllers.owner_controller import owners_blueprint
 from controllers.appointment_controller import appointments_blueprint
 from controllers.animal_controller import animals_blueprint
@@ -12,7 +16,10 @@ app.register_blueprint(animals_blueprint)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    owners = owner_repository.select_all()
+    animals = animal_repository.select_all()
+    appointments = appointment_repository.select_all()
+    return render_template('index.html', owners=owners, animals=animals, appointments=appointments)
 
 if __name__ == '__main__':
     app.run
